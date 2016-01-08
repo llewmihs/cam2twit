b7791d8cfd07f440d762fd71f26d7a4d9eb4f1f4
llewmihs <joeshimwell@gmail.com>
2016-01-08 13:53:26 +0000

    Update Cam2Twit_conf.py

--- Cam2Twit_conf.py
+++ Cam2Twit_conf.py
@@ -1,1 +1,6 @@
-print 'test'
+#Details for Twitter
+
+app_key = "INSERT APP KEY HERE"
+app_secret = "INSERT APP SECRET KEY HERE"
+oauth_token = "INSERT OAUTH TOKEN HERE"
+oauth_token_secret = "INSERT OAUTH TOEKN SECRET HERE"
